3158992f2f5f517ea4fb5f73308b5fa7121a8199
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2020-09-02 23:16:56 +0530

    Updates

---
+++ precache-manifest.20729dfa245b88efaba18fd0ce71ca62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f110cc3f51cb209d647e386bcdf1b36",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "9c463e80c09b7c5a756a",
+    "url": "/my-first-react-app/static/css/main.56430209.chunk.css"
+  },
+  {
+    "revision": "07be3335193fd3e01547",
+    "url": "/my-first-react-app/static/js/2.b2300469.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.b2300469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c463e80c09b7c5a756a",
+    "url": "/my-first-react-app/static/js/main.23422aef.chunk.js"
+  },
+  {
+    "revision": "35dd41cdf223434ae6cf",
+    "url": "/my-first-react-app/static/js/runtime-main.d382a63a.js"
+  }
+]);
